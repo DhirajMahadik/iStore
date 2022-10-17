bea7c4fac183d85f1fdf72340c5474a3617a6a8a
dhiraj <dhirajmahadik9221@gmail.com>
2022-10-17 11:35:57 +0530

    fourth

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
-import Mens from './Components/Mens';
+import Collections from './Components/Collections';
 import Footer from './Components/Footer';
 
 function App() {
@@ -16,9 +16,9 @@ function App() {
     <Navbar/>
     <Routes>
     <Route path="/" element={<Home/>}/>
-    <Route path="/Mens" element={<Mens category={"men's clothing"} heading={"Men's Collection"}/>}/>
-    <Route path="/Woman" element={<Mens category={"women's clothing"} heading={"Woman's Collection"}/>}/>
-    <Route path="/Accessories" element={<Mens category={"electronics"} heading={"Accessories Collection"}/>}/>
+    <Route path="/Mens" element={<Collections category={"men's clothing"} heading={"Men's Collection"}/>}/>
+    <Route path="/Woman" element={<Collections category={"women's clothing"} heading={"Woman's Collection"}/>}/>
+    <Route path="/Accessories" element={<Collections category={"electronics"} heading={"Accessories Collection"}/>}/>
     </Routes>
     <Footer/>
     </BrowserRouter>
